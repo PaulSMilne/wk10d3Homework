7d2d105d1f2cd21563c595c03f42a602720be1df
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-30 12:06:37 +0000

    wrote test for replenishment value

--- specs/food_spec.js
+++ specs/food_spec.js
@@ -6,10 +6,14 @@ describe("Food", function(){
     var honey = null;
 
     beforeEach( function() {
-        honey = new Food( "honey" );
+        honey = new Food( "honey", 30 );
     });
 
     it("has name passed in constructor", function(){
         assert.equal("honey", honey.name);
     });
-})
+
+    it("should have repleishmentValue passed in constructor", function() {
+        assert.equal( 30, honey.repleishmentValue );
+    });
+});
