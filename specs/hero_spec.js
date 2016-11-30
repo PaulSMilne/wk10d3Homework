1802ced94a1bdf56b05644b93d2727a8e399e672
Paul Milne <hallhill@gmail.com>
2016-11-30 11:46:50 +0000

    Created a hero constructor method on hero.js to pass the first test

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,6 +1,12 @@
 var assert = require('assert');
+var Hero = require('../hero');
 
 describe( "Hero", function() {
+
+     var hero = null;
+     beforeEach(function(){
+          hero = new Hero("Hercules");
+     })
 
   it( "has name variable", function() {
 
