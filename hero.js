74f11b547e3fe00baf5d7366bf0f6cf7bf0b568b
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-30 14:59:09 +0000

    adding logging to hero

--- hero.js
+++ hero.js
@@ -9,23 +9,28 @@ Hero.prototype = {
           return "Hi, my name is " + this.name;
      },
      eat: function( food ) {
+         console.log( "Hero: omm nom nom I'm eating", food );
          var replenishmentFactor = 1;
          if (food.name === this.favouriteFood && food.replenishmentValue > 0){
           replenishmentFactor = 1.5;
          }
-         this.health += food.replenishmentValue * replenishmentFactor;   
+         this.health += food.replenishmentValue * replenishmentFactor;
     },
     checkIfPrincess: function(candidate){
           if (candidate.hat === "crown"){
+              console.log( "Hero: Yesss! I have found a princess!" );
                return true;
           }
+          console.log( "Hero: You are no princess!" );
           return false;
     },
     save: function(candidate){
           var isPrincess = this.checkIfPrincess(candidate);
           if (isPrincess){
+              console.log( "Hero: Don't worry missus, I will save you" );
                candidate.inPeril = false;
           }
+          console.log("Hero: I shall not save you, you are not worthy!" );
     }
 }
 
