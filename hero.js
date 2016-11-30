88e4c489774ac83eb8a1ff1aadddcbb9a6838699
Paul Milne <hallhill@gmail.com>
2016-11-30 11:57:29 +0000

    wrote test and method for the hero saying their name

--- hero.js
+++ hero.js
@@ -1,6 +1,13 @@
-var Hero = function( name ){
+var Hero = function( name, favouriteFood ){
      this.name = name;
      this.health = 100;
+     this.favouriteFood = favouriteFood;
+};
+
+Hero.prototype = {
+     talk: function(){
+          return "Hi, my name is " + this.name;
+     }
 }
 
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -6,7 +6,7 @@ describe( "Hero", function() {
      var hero = null;
      beforeEach(function(){
           hero = new Hero("Hercules", "steak");
-     })
+     });
 
   it( "has name variable passed in constructor", function() {
 
@@ -15,9 +15,13 @@ describe( "Hero", function() {
 
   it("has health that starts at 100", function(){
      assert.equal(100, hero.health);
-  })
+  });
 
   it( "has a favourite food passed in constructor", function() {
     assert.equal("steak", hero.favouriteFood );
+  });
+
+  it("can say their name", function(){
+     assert.equal("Hi, my name is Hercules", hero.talk());
   });
 });
